155a925fcc9dd3cf218201b087455ec0fb780fd6
Vaggelis Magonezos <info@app-ify.com>
2025-09-05 19:02:33 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+//---------------------------------------------------------------
+
 export default function Home() {
   return (
     <div className=" items-center justify-center flex min-h-screen bg-gray-950">
